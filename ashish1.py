a97a289c14179b3ab8e460e135ba51428d9b25a7
AshishSat22 <aksatapathy2006@gmail.com>
2025-02-15 15:28:56 +0530

    add coded

--- ashish1.py
+++ ashish1.py
@@ -1,8 +1,8 @@
 print("india")
-
+print("Jai Hind0")
 print("india")
 print("india")
 print("india")
 st=[1,2,3,4,5,6]
-s=len(st)
-print("im learning git hub")
+for i in range(1,6):
+    print("jai hind")
